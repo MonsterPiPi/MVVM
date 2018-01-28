c32e52a88193297cbe9333b29907534cca4670a2
iLiQian <51103942@qq.com>
2018-01-28 14:17:32 +0800

    初始化MVVM

---
+++ app/src/main/java/example/com/mvvm/MainActivity.java
@@ -0,0 +1,20 @@
+package example.com.mvvm;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import example.com.mvvm.bean.User;
+import example.com.mvvm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //用 DatabindingUtil.setContentView() 来替换掉 setContentView()
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("testFirst", "testLast");
+        binding.setUser(user);
+    }
+}
